73b2f5c219af3b9c2501276c1363cb4f5f31e1d0
X Wang <24860335+xwang233@users.noreply.github.com>
2020-03-19 17:09:41 -0700

    nvprof parser test

---
+++ nvprof/profile.py
@@ -0,0 +1,34 @@
+import tempfile
+import subprocess
+import torch
+
+def nvprof_parser_test():
+    PYTHON = '/usr/bin/python3'
+    NVPROF = '/usr/local/cuda/bin/nvprof'
+
+    fp_python = tempfile.NamedTemporaryFile()
+    fp_python.write("""
+import torch
+with torch.cuda.profiler.profile():
+    with torch.autograd.profiler.emit_nvtx():
+        x = torch.randn(4, dtype=torch.float, device='cuda')
+        net = torch.nn.ReLU().cuda()
+        y = net(x).sum()""".encode('ascii'))
+    fp_python.flush()
+
+    fp_nvprof = tempfile.NamedTemporaryFile()
+
+    subprocess.run([NVPROF, '-o', fp_nvprof.name, '-f', '--', PYTHON, fp_python.name], check=True)
+
+    prof = torch.autograd.profiler.parse_nvprof_trace(fp_nvprof.name)
+
+    assert isinstance(prof, list)
+    assert len(prof) == 2
+    assert prof[0].name.startswith('relu')
+    assert prof[1].name.startswith('sum')
+
+    fp_nvprof.close()
+    fp_python.close()
+
+if __name__ == "__main__":
+    nvprof_parser_test()
